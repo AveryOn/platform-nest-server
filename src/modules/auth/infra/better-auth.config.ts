ab4c03c8d59146accc942f7c65026e9e9c4a42ed
AveryOn <lnkonion2@gmail.com>
2026-04-11 22:16:42 +0400

    init better auth config

--- src/modules/auth/infra/better-auth.config.ts
+++ src/modules/auth/infra/better-auth.config.ts
@@ -1,4 +1,47 @@
+import { drizzleAdapter } from 'better-auth/adapters/drizzle'
+import { bearer, openAPI, organization } from 'better-auth/plugins'
+
+import { env } from '~/core/env'
+import { db } from '~/infra/drizzle/client'
+import * as schema from '~/infra/drizzle/schemas/auth-schema'
+
 export const betterAuthConfig = {
-  basePath: '/api/v1/auth',
-  baseURL: 'http://localhost:3000',
+  appName: 'UI Rules',
+  secret: env.BETTER_AUTH_SECRET,
+  baseURL: env.BETTER_AUTH_URL,
+  basePath: env.BETTER_AUTH_BASE_PATH,
+
+  database: drizzleAdapter(db, {
+    provider: 'pg',
+    usePlural: true,
+    schema,
+  }),
+
+  trustedOrigins: env.CORS_ORIGIN,
+
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: true,
+  },
+
+  session: {
+    expiresIn: 60 * 60 * 24 * 7,
+    updateAge: 60 * 60 * 24,
+  },
+
+  advanced: {
+    cookiePrefix: 'better-auth',
+    crossSubDomainCookies: {
+      enabled: false,
+    },
+  },
+
+  socialProviders: {
+    google: {
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    },
+  },
+
+  plugins: [organization(), bearer(), openAPI()],
 }
