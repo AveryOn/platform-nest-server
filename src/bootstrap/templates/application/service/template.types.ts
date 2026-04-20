a3a01978e5447dece11e044544c6b0dc55469c72
AveryOn <lnkonion2@gmail.com>
2026-04-20 16:14:42 +0400

    added shadcn template to bootstrap/templates

---
+++ src/bootstrap/templates/application/service/template.types.ts
@@ -0,0 +1,36 @@
+export enum RuleGroupType {
+  category,
+  token,
+  section,
+  component,
+  variant,
+}
+export type RuleGroupTypeKey = keyof typeof RuleGroupType
+
+export type JsonBody<T> = Record<string, T>
+
+export interface TemplateRule {
+  ref: string
+  key: string // stable identifier (build-time only)
+  title?: string
+  orderIndex: number
+  body: JsonBody<any> | string
+  metadata?: JsonBody<any>
+}
+
+export interface TemplateGroup {
+  ref: string
+  type: RuleGroupTypeKey
+  key: string // stable identifier (build-time only)
+  name: string
+  orderIndex: number
+  children?: TemplateGroup[]
+  rules?: TemplateRule[]
+  metadata?: JsonBody<any>
+}
+
+export interface TemplateBase {
+  slug: string
+  name: string
+  groups: TemplateGroup[]
+}
